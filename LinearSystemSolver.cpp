fdc681371b31ffb7188d15b1f829697424346f08
andrewsanford <111091439+andrewsanford@users.noreply.github.com>
2022-09-15 14:13:32 -0700

    Update LinearSystemSolver.cpp

--- LinearSystemSolver.cpp
+++ LinearSystemSolver.cpp
@@ -24,7 +24,7 @@ int getSize(string fileNamePar) {
 }
 
 vector<double> getLastLine(int vectorSizePar, string fileNamePar) {
-	double dump; ;
+	double dump;
 	ifstream inputFile;
 	vector<double> returnVector;
 
